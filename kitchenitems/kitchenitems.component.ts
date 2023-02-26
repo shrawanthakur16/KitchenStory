6be8fc5fea97a29f97d7b14541bee6ee8f90880d
shrawanthakur16 <54633307+shrawanthakur16@users.noreply.github.com>
2023-02-26 15:52:49 +0530

    Update kitchenitems.component.ts

--- kitchenitems/kitchenitems.component.ts
+++ kitchenitems/kitchenitems.component.ts
@@ -8,6 +8,7 @@ import {Router} from '@angular/router';
   templateUrl: './kitchenitems.component.html',
   styleUrls: ['./kitchenitems.component.css']
 })
+
 export class KitchenitemsComponent implements OnInit {
 
   
